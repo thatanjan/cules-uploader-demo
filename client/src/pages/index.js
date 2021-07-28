d3c51733b809e41460a835f55dd2ad60b4d91c99
thatanjan <anjan.cules@gmail.com>
2021-07-28 11:38:58 +0600

    title aded to the page

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,3 +1,21 @@
+import Typography from '@material-ui/core/Typography'
+import Grid from '@material-ui/core/Grid'
+
 export default function Home() {
-	return <div>Hello world</div>
+	return (
+		<Grid container justifyContent='center'>
+			<Grid
+				item
+				xs={12}
+				md={7}
+				sx={{ display: 'grid', placeItems: 'center', height: '15vh' }}
+			>
+				<Typography variant='h2' color='primary'>
+					Cules Uploader
+				</Typography>
+			</Grid>
+
+			<Grid item xs={12} md={7}></Grid>
+		</Grid>
+	)
 }
