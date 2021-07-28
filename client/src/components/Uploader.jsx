e5743055fa225baa20632f0db491a0aca1849759
thatanjan <anjan.cules@gmail.com>
2021-07-28 15:43:57 +0600

    file upload state created, image is shown

--- client/src/components/Uploader.jsx
+++ client/src/components/Uploader.jsx
@@ -1,54 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useDropzone } from 'react-dropzone'
 import Card from '@material-ui/core/Card'
+import Grid from '@material-ui/core/Grid'
 import CardContent from '@material-ui/core/CardContent'
+import CardMedia from '@material-ui/core/CardMedia'
+import CardActions from '@material-ui/core/CardActions'
 import Typography from '@material-ui/core/Typography'
 
 import PublishIcon from '@material-ui/icons/Publish'
 
 const Uploader = () => {
+	const [base64File, setBase64File] = useState('')
+	const [previewLink, setPreviewLink] = useState('')
+	const [upload, setUpload] = useState({
+		uploading: false,
+		success: false,
+		failed: false,
+	})
+
 	const { getRootProps, getInputProps } = useDropzone({
-		accept: 'image/*, video/*',
+		accept: 'image/*',
 		maxFiles: 1,
 		onDrop: (acceptedFiles) => {
 			const realFile = acceptedFiles[0]
-			const file = realFile
 
 			const reader = new FileReader()
 
-			reader.onload = () => {
-				const img = new Image()
-				img.onload = function () {
-					console.log('The width of the image is ' + img.width + 'px.')
-				}
-				img.src = reader.result
-				console.log(img)
-			}
+			const previewLink = URL.createObjectURL(realFile)
+
+			setPreviewLink(previewLink)
 
 			reader.readAsDataURL(realFile)
 
-			const fileWithPreviewLink = Object.assign(realFile, {
-				previewLink: URL.createObjectURL(realFile),
-				valid: true,
-			})
+			reader.onload = () => {
+				setBase64File(reader.result)
+			}
 		},
 	})
 
 	return (
 		<Card>
-			<CardContent
-				sx={{
-					height: '20rem',
-					display: 'grid',
-					justifyItems: 'center',
-					gridGap: '1rem',
-				}}
-				{...getRootProps()}
-			>
-				<input {...getInputProps()} />
-				<PublishIcon fontSize='large' sx={{ alignSelf: 'end' }} />
-				<Typography>Drag and Drop to upload</Typography>
-			</CardContent>
+			{!previewLink && (
+				<CardContent
+					sx={{
+						height: '20rem',
+						display: 'grid',
+						justifyItems: 'center',
+						gridGap: '1rem',
+					}}
+					{...getRootProps()}
+				>
+					<input {...getInputProps()} />
+					<PublishIcon fontSize='large' sx={{ alignSelf: 'end' }} />
+					<Typography>Drag and Drop to upload</Typography>
+				</CardContent>
+			)}
+
+			{previewLink && (
+				<CardMedia image={previewLink} sx={{ height: 0, paddingTop: '56.25%' }} />
+			)}
+
+			<CardActions> </CardActions>
 		</Card>
 	)
 }
