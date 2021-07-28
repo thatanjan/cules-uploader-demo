785780abae75ff034b684c92d10cb3e3054585c7
thatanjan <anjan.cules@gmail.com>
2021-07-28 16:49:53 +0600

    get all images route is created

---
+++ client/src/pages/api/getAllImages.js
@@ -0,0 +1,31 @@
+import cloudinary from 'cloudinary'
+
+import uploadDir from './upload'
+
+const handler = async ({ method }, res) => {
+	try {
+		if (method !== 'GET')
+			return res.status(400).json({ message: 'Invalid method' })
+
+		const cloudinaryV2 = cloudinary.v2
+
+		cloudinaryV2.config({
+			cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+			api_key: process.env.CLOUDINARY_API_KEY,
+			api_secret: process.env.CLOUDINARY_API_SECRET,
+		})
+
+		const images = await cloudinaryV2.api.resources({
+			type: 'upload',
+			prefix: uploadDir,
+		})
+
+		const public_ids = images.resources.map((image) => image.public_id)
+
+		return res.json({ images: public_ids })
+	} catch (e) {
+		return res.status(400).send({ message: 'Error processing request' })
+	}
+}
+
+export default handler
